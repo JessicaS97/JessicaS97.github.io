3046e70b168cbf78028c2b423e58395f80b5a65a
Jessica J Sun <jessi.slm@hotmail.com>
2021-03-12 12:55:42 +1100

    Delete

--- README.md
+++
@@ -1,1 +0,0 @@
-Website link https://jessicajsportfolio.github.io/
--- demo.java
+++
@@ -1,9 +0,0 @@
-import java.applet.*;
-impor java.awt.*;
-public class appletdemo extends Applet
-{
- public void paint(Graphics g)
- {
- g.drawString("this is code from java",10,10);
- }
-}
